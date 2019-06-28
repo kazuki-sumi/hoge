24be845e17fd30ac2d0a3b49261f7025db23f9ea
kazuki sumi <cs11600@yahoo.co.jp>
2019-06-28 22:56:25 +0900

    first_commit

---
+++ lib/hoge.rb
@@ -0,0 +1,6 @@
+require "hoge/version"
+
+module Hoge
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/hoge_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Hoge do
+  it "has a version number" do
+    expect(Hoge::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
